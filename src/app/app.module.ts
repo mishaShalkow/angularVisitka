84409ff3f85c9f1c89898a90d6b7e98cb80adada
[MSHa] <[misha.shalkow@gmail.com]>
2022-06-24 19:11:09 +0300

    conect git hub to a project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainScreenComponent } from './mainScreen/mainScreen.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, MainScreenComponent, NavigationComponent],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/mainScreen/mainScreen.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mainScreen',
+  templateUrl: './mainScreen.component.html',
+  styleUrls: ['./mainScreen.component.scss'],
+})
+export class MainScreenComponent {}
