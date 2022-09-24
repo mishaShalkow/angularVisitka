28a93516031f03b0ade6098b6e13c11c057a2d5e
mishaShalkow <misha.shalkow@gmail.com>
2022-09-24 14:29:58 +0300

    add filter_pipes

--- src/app/card-buy/card-buy.component.ts
+++ src/app/card-buy/card-buy.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
 import { CartServiceService } from '../Service/cart-service.service';
 import { IProduct } from '../models/cardProduct';
+import { products } from '../data/cardObj';
 
 @Component({
   selector: 'app-card-buy',
@@ -9,17 +10,26 @@ import { IProduct } from '../models/cardProduct';
 })
 export class CardBuyComponent implements OnInit {
   items;
+  products;
   formsBuy = false;
   @Input() title = 'Информация для заказа';
   @Output() close = new EventEmitter<void>();
+  totalPrice: any = 0;
+  quantity = 1;
 
   clearCart() {
     this.items = [];
     return this.items;
   }
 
+  getTotalSum() {
+    this.totalPrice += this.totalPrice;
+  }
+
   constructor(private cartServiceService: CartServiceService) {
     this.items = this.cartServiceService.getItems();
+    /*  this.products = this.cartServiceService.getSum(this.price); */
+
     /* this.items = this.cartServiceService.clearCart(); */
   }
   /*   getsSum(products: IProduct[]) {} */
